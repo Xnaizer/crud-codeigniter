47775de57aceba8be02d3bbbdc89edf92d37da59
Xnaizer <seykig028@gmail.com>
2026-04-14 20:13:18 +0700

    (feat: route): adding login and register route to the app

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,19 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+
+
+$routes->get('/', function() {
+    return view('home_view');
+});
+
+$routes->get('/auth/login', function() {
+    return view('login_view');
+});
+
+$routes->get('/auth/register', function() {
+    return view('register_view');
+});
+
+$routes->post('login', 'Auth::login');
+$routes->post('register', 'Auth::register');
